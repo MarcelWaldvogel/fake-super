c92feb462ebde999d28ffb572a76db83173e750f
Marcel Waldvogel <marcel.waldvogel@trifence.ch>
2021-11-14 15:15:22 +0100

    🎨 Split tests

--- fake_super/tests/11-statfmt-tests.py
+++ fake_super/tests/11-statfmt-tests.py
@@ -12,29 +12,41 @@ def assertEqual(a, b):
             % (a, type(a), b, type(b)))
 
 
-def test_statfmt():
+def test_statfmt1():
     stat = {
         'mode': 0o100444, 'type': 'reg', 'perms': 0o0444,
         'major': 0, 'minor': 0, 'owner': 0, 'group': 0
     }
     assertEqual(statfmt(stat), "regular file, permissions 0444, owner 0:0")
+
+
+def test_statfmt2():
     stat = {
         'mode': 0o107444, 'type': 'reg', 'perms': 0o7444,
         'major': 9, 'minor': 8, 'owner': 7, 'group': 6
     }
     assertEqual(statfmt(stat), "regular file, permissions 7444, owner 7:6")
+
+
+def test_statfmt3():
     stat = {
         'mode': 0o020444, 'type': 'chr', 'perms': 0o0444,
         'major': 1, 'minor': 2, 'owner': 3, 'group': 4
     }
     assertEqual(statfmt(stat),
                 "character device (1,2), permissions 0444, owner 3:4")
+
+
+def test_statfmt4():
     stat = {
         'mode': 0o060444, 'type': 'blk', 'perms': 0o0444,
         'major': 1, 'minor': 2, 'owner': 3, 'group': 4
     }
     assertEqual(statfmt(stat),
                 "block device (1,2), permissions 0444, owner 3:4")
+
+
+def test_statfmt5():
     stat = {
         'mode': 0o040444, 'type': 'dir', 'perms': 0o0444,
         'major': 1, 'minor': 2, 'owner': 3, 'group': 4
